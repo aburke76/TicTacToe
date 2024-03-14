1384baa72e50fa643e9e256e6100d031322ef61d
aburke76 <alexlburke8771@gmail.com>
2024-03-14 13:47:02 -0400

    Gameboard object

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 //create object function that runs the game
 
 //NOTE: return any functions that need to be used outside of their module!!
+
+const Gameboard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const render = () => {
+        let htmlBoard = "";
+        board.forEach((square, index) => {
+            htmlBoard += `<div class='square' id='square-${index}'>${square}</div>`;
+        });
+        document.querySelector("#gameboard-container").innerHTML = htmlBoard;
+    };
+    return { render };
+})();
