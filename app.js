a0c831924f202e6ba7be3bdfcab826c7d49306f8
aburke76 <alexlburke8771@gmail.com>
2024-03-14 15:08:47 -0400

    starting over, working slower

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-//create Gameboard object, store gameboard array inside, render board on page
+//create Gameboard object, store gameboard array inside
 //create factory function for creating players
 //create object function that runs the game
 
 //NOTE: return any functions that need to be used outside of their module!!
 
 const Gameboard = (() => {
-    let board = ["", "", "", "", "", "", "", "", ""];
-
-    const render = () => {
-        let htmlBoard = "";
-        board.forEach((square, index) => {
-            htmlBoard += `<div class='square' id='square-${index}'>${square}</div>`;
-        });
-        document.querySelector("#gameboard-container").innerHTML = htmlBoard;
+    let gameboard = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ];
+    return {
+        gameboard,
     };
-    return { render };
+})();
+
+const Player = (name, marker) => {
+    return { name, marker };
+};
+
+const Game = (() => {
+    const player1 = Player(document.querySelector("#player1").value, "X");
+    const player2 = Player(document.querySelector("#player2").value, "O");
+    let currentPlayer = player1;
+    //create eventListeners on board, start new round after every click
+    //check is cell is empty. if so, add player marker to cell on gameboard, new round
+    //check gameboard for a win for currentPlayer, or draw. if no win, switch players
 })();
