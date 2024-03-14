63e3a89270d237f48130467a43c31812aa79e125
aburke76 <alexlburke8771@gmail.com>
2024-03-14 11:28:07 -0400

    psuedocode

---
+++ app.js
@@ -0,0 +1,5 @@
+//create Gameboard object, store gameboard array inside, render board on page
+//create factory function for creating players
+//create object function that runs the game
+
+//NOTE: return any functions that need to be used outside of their module!!
